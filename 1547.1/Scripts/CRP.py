41a0b3915d273be7981f44b252b902d6b4f5e647
MBui <michel.bui@canada.ca>
2019-11-05 14:01:34 -0500

    Fixing Typo's in versioning to 1.2.2
    Fixed typo in versioning to Version 1.2.2

--- 1547.1/Scripts/CRP.py
+++ 1547.1/Scripts/CRP.py
@@ -553,7 +553,7 @@ def run(test_script):
     sys.exit(rc)
 
 
-info = script.ScriptInfo(name=os.path.basename(__file__), run=run, version='1.2.1')
+info = script.ScriptInfo(name=os.path.basename(__file__), run=run, version='1.2.2')
 
 # CPF test parameters
 info.param_group('crp', label='Test Parameters')
--- 1547.1/Scripts/FW.py
+++ 1547.1/Scripts/FW.py
@@ -414,7 +414,7 @@ def run(test_script):
 
     sys.exit(rc)
 
-info = script.ScriptInfo(name=os.path.basename(__file__), run=run, version='1.2.1')
+info = script.ScriptInfo(name=os.path.basename(__file__), run=run, version='1.2.2')
 
 # FW test parameters
 info.param_group('fw', label='Test Parameters')
